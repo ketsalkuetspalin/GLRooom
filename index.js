dc2f51aef85dcca0ce3ebcaa42f52b191a8ba9d5
emadrid <emadrid@resuelve.mx>
2017-08-04 20:09:53 -0500

    Peticiones de servicio

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -7,9 +6,60 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
-  socket.on('service', (data) => socket.broadcast.emit('service', data));
-}
+var clients = [];
+var services = {};
+
+function onConnection(socket) {
+    console.info('New User connected (id=' + socket.id + ').');
+    clients.push(socket);
+
+    // Direct messages
+    socket.on('room', function(room) {
+        socket.join(room);
+    });
+
+    // Services
+    socket.on('ServiceRequest', function(data) {
+        console.log("OnService broadcast!!", data);
+        services[data] = false;
+        socket.broadcast.emit('ServiceAvailable', data);
+
+    });
+
+    socket.on("AcceptService", function(payload) {
+
+        console.log("Is service still available?", services[payload.service] == false);
+
+        if (services[payload.service] === false) {
+            console.log("Service accepted !!", payload);
+            services[payload.service] = true;
+            io.sockets.in(payload.user).emit('AcceptService', 'it´s yours`');
+        } else {
+            io.sockets.in(payload.user).emit('AcceptService', 'sorry!');
+        }
+
+    });
+
+
+    // Users
+    socket.on('LoginGlker', function(data1) {
+        console.log('New glker connected : ' + data1);
+    });
+
+    socket.on('LogoutGlker', function(data) {
+        console.log('Adios' + data);
+    });
+
+    socket.on('disconnect', function(data) {
+
+        var index = clients.indexOf(socket);
+        if (index != -1) {
+            clients.splice(index, 1);
+            console.info('Client gone (id=' + socket.id + ').');
+        }
+    });
+
+};
 
 io.on('connection', onConnection);
 
--- public/main.js
+++ public/main.js
@@ -2,99 +2,94 @@
 
 (function() {
 
-  var socket = io();
-  var canvas = document.getElementsByClassName('whiteboard')[0];
-  var colors = document.getElementsByClassName('color');
-  var context = canvas.getContext('2d');
+    var colors = document.getElementsByClassName('color');
 
-  var current = {
-    color: 'black'
-  };
-  var service = false;
-
-  canvas.addEventListener('mousedown', onMouseDown, false);
-  canvas.addEventListener('mouseup', onMouseUp, false);
-  canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-
-  for (var i = 0; i < colors.length; i++){
-    colors[i].addEventListener('click', onColorUpdate, false);
-  }
-
-  socket.on('service', onServiceEvent);
-
-  window.addEventListener('resize', onResize, false);
-  onResize();
-
-
-  function drawLine(x0, y0, x1, y1, color, emit){
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2;
-    context.stroke();
-    context.closePath();
-
-    if (!emit) { return; }
-    var w = canvas.width;
-    var h = canvas.height;
-
-    socket.emit('service', {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color
+    var socket1 = io("http://localhost:3000");
+    var socket2 = io('http://localhost:3000', {
+        forceNew: true
     });
-  }
+    const sysSocket = io('http://localhost:3000', {
+        forceNew: true
+    });
 
-  function onMouseDown(e){
-    service = true;
-    current.x = e.clientX;
-    current.y = e.clientY;
-  }
+    var service = false;
 
-  function onMouseUp(e){
-    if (!service) { return; }
-    service = false;
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
-  }
+    for (var i = 0; i < colors.length; i++) {
+        colors[i].addEventListener('click', onColorUpdate, false);
+    }
 
-  function onMouseMove(e){
-    if (!service) { return; }
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
-    current.x = e.clientX;
-    current.y = e.clientY;
-  }
-
-  function onColorUpdate(e){
-    current.color = e.target.className.split(' ')[1];
-  }
-
-  // limit the number of events per second
-  function throttle(callback, delay) {
-    var previousCall = new Date().getTime();
-    return function() {
-      var time = new Date().getTime();
-
-      if ((time - previousCall) >= delay) {
-        previousCall = time;
-        callback.apply(null, arguments);
-      }
+    function onColorUpdate(e) {
+        var color = e.target.className.split(' ')[1];
+        console.log(color);
+        sysSocket.emit('ServiceRequest', color);
     };
-  }
 
-  function onServiceEvent(data){
-    var w = canvas.width;
-    var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-  }
+    socket1.on('connect', function() {
 
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+        console.log("Connected");
+
+        var person_name = "fernanda";
+
+        socket1.emit('room', person_name);
+
+        socket1.emit('LoginGlker', person_name);
+
+        socket1.on('disconnected', function() {
+
+            socket1.emit('LogoutGlker', person_name);
+        });
+
+        socket1.on("ServiceAvailable", function(data) {
+            console.log(person_name, "Service available", data);
+            socket2.emit("AcceptService", {
+                user: person_name,
+                service: data
+            });
+
+        });
+
+        // En servicio
+        socket1.on("AcceptService", function(payload) {
+            console.log("Accepted", payload);
+        });
+
+    });
+
+
+    socket2.on('connect', function() {
+
+        console.log("Connected");
+
+        var person_name = "Jorge";
+
+        socket2.emit('room', person_name);
+
+        socket2.emit('LoginGlker', person_name);
+
+        socket2.on('disconnected', function() {
+            socket2.emit('LogoutGlker', person_name);
+        });
+
+        socket2.on("ServiceAvailable", function(data) {
+            console.log(person_name, "Service available", data);
+
+            socket2.emit("AcceptService", {
+                user: person_name,
+                service: data
+            });
+
+        });
+
+        // En servicio
+        socket2.on("AcceptService", function(payload) {
+            console.log("Accepted", payload);
+        });
+
+    });
+
+    function onServiceEvent(data) {
+        console.log("Service received", data);
+    };
+
 
 })();
